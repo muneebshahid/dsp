905e006602a5c0724ce832d1248c4dfef9720db4
Muneeb Shahid <muneebshahid5@gmail.com>
2016-01-20 11:39:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ using namespace dynamic_shortest_path;
 
 std::vector<std::vector<float> > read_graph()
 {
-	std::ifstream scores("C:\\Users\\Muneeb\\workspace\\dsp\\src\\scores.txt");
+	std::ifstream scores("scores.txt");
 	std::string line;
 	std::vector<std::vector<float> > lines;
 	while (std::getline(scores, line))
@@ -39,14 +39,14 @@ std::vector<std::vector<float> > read_graph()
 int main()
 {
 	std::vector<std::vector<float> > lines = read_graph();
-	Dsp path(3, .05, 3, lines[0].size());
+	Dsp dsp(3, .05, 3, lines[0].size());
 	for(std::vector<int>::size_type i = 0; i != lines.size(); i++)
 	{
-		path.forward(lines[i]);
-		path.print();
+		dsp.forward(lines[i]);
+		dsp.print();
 		std::cout<<"\n----------\n";
 	}
-	Path shortest_path = path.backward(2);
+	Path shortest_path = dsp.backward(2);
 	shortest_path.print(false);
 	return 0;
 }
